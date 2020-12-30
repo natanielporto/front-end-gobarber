5b785e5c9b05199699a267cffce2bdb18203cb1d
Nataniel Porto <natanielkporto@gmail.com>
2020-12-30 19:29:53 -0300

    final project

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,65 @@
+import React, {
+  InputHTMLAttributes,
+  useEffect,
+  useRef,
+  useState,
+  useCallback,
+} from 'react';
+import { useField } from '@unform/core';
+import { IconBaseProps } from 'react-icons';
+import { FiAlertCircle } from 'react-icons/fi';
+
+import { Container, Error } from './styles';
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  name: string;
+  icon?: React.ComponentType<IconBaseProps>;
+}
+
+const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest }) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const [focused, setFocused] = useState(false);
+  const [filled, setFilled] = useState(false);
+
+  const { fieldName, defaultValue, error, registerField } = useField(name);
+
+  const handleSetFocused = useCallback(() => {
+    setFocused(true);
+  }, []);
+
+  const handleInputBlur = useCallback(() => {
+    setFocused(false);
+
+    setFilled(!!inputRef.current?.value);
+  }, []);
+
+  useEffect(() => {
+    registerField({
+      name: fieldName,
+      ref: inputRef.current,
+      path: 'value',
+    });
+  }, [fieldName, registerField]);
+
+  return (
+    <Container hasError={!!error} filled={filled} focused={focused}>
+      {Icon && <Icon size={20} />}
+      <input
+        onFocus={handleSetFocused}
+        onBlur={handleInputBlur}
+        defaultValue={defaultValue}
+        ref={inputRef}
+        {...rest}
+      />
+
+      {error && (
+        <Error title={error}>
+          <FiAlertCircle color="#c53030" size={20} />
+        </Error>
+      )}
+    </Container>
+  );
+};
+
+export default Input;
---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Dashboard: React.FC = () => {
+  return (
+    <h1>
+      oi
+      <h2>olá</h2>
+    </h1>
+  );
+};
+
+export default Dashboard;
